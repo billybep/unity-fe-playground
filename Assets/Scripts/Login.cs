f654ddf88f56a00a438b5516e4e1ed2a7bcf3006
billyep <billypesoth@bigdade.id>
2023-01-20 23:59:01 +0800

    implement register account

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-    [SerializeField] private string _authEndpoint = "http://127.0.0.1:13765/account";
-    
+    [SerializeField] private string _authEndpointLogin = "http://127.0.0.1:13765/account/login";
+    [SerializeField] private string _authEndpointRegister = "http://127.0.0.1:13765/account/register";
+
     [SerializeField] private TextMeshProUGUI _statusText;
     [SerializeField] private Button _loginButton;
+    [SerializeField] private Button _registerButton;
     [SerializeField] private TMP_InputField _usernameInputField;
     [SerializeField] private TMP_InputField _passwordInputField;
 
@@ -18,9 +20,18 @@ public class Login : MonoBehaviour
     {
         _statusText.text = "Signing in ...";
         _loginButton.interactable = false;
-        Debug.Log(_authEndpoint);
+        Debug.Log(_authEndpointLogin);
 
         StartCoroutine(TryLogin());
+    }
+
+    public void OnRegisterClick()
+    {
+        _statusText.text = "Creating account ...";
+        _registerButton.interactable = false;
+        Debug.Log(_authEndpointRegister);
+
+        StartCoroutine(TryRegister());
     }
 
     private IEnumerator TryLogin()
@@ -43,7 +54,11 @@ public class Login : MonoBehaviour
             yield break;
         }
 
-        UnityWebRequest request = UnityWebRequest.Get($"{_authEndpoint}?username={username}&password={password}");
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(_authEndpointLogin, form);
         var handler = request.SendWebRequest();
 
         float startTime = 0.0f;
@@ -59,30 +74,6 @@ public class Login : MonoBehaviour
 
             yield return null;
         }
-
-        // if Web request successfull
-        //if (request.result == UnityWebRequest.Result.Success)
-        //{
-        //    // if Success login
-        //    if (request.downloadHandler.text != "Invalid credential")
-        //    {
-        //        _statusText.text = "Welcome";
-        //        _loginButton.interactable = false;
-        //    }
-        //    else
-        //    {
-        //        _statusText.text = "Invalid credential";
-        //        _loginButton.interactable = true;
-        //    }
-
-        //    _statusText.text = request.downloadHandler.text;
-        //    _loginButton.interactable = true;
-        //}
-        //else
-        //{
-        //    _statusText.text = "Error connection to the server...";
-        //    _loginButton.interactable = true;
-        //}
 
         switch (request.result)
         {
@@ -107,16 +98,84 @@ public class Login : MonoBehaviour
                     _loginButton.interactable = true;
                 }
 
-                //_statusText.text = request.downloadHandler.text + "WELCOME";
-                //_loginButton.interactable = true;
-
                 // Clear
                 _usernameInputField.text = null;
-                _passwordInputField = null;
+                _passwordInputField.text = null;
                 break;
         }
 
-        Debug.Log($"{username}:{password}");
+        yield return null;
+    }
+
+    private IEnumerator TryRegister()
+    {
+        string username = _usernameInputField.text;
+        string password = _passwordInputField.text;
+
+        // check input
+        if (username.Length < 3 || username.Length > 24)
+        {
+            _statusText.text = "Invalid username";
+            _registerButton.interactable = true;
+            yield break;
+        }
+
+        if (password.Length < 3 || password.Length > 24)
+        {
+            _statusText.text = "Invalid password";
+            _registerButton.interactable = true;
+            yield break;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+
+        UnityWebRequest request = UnityWebRequest.Post(_authEndpointRegister, form);
+        var handler = request.SendWebRequest();
+
+        float startTime = 0.0f;
+
+        while (!handler.isDone)
+        {
+            startTime += Time.deltaTime;
+
+            if (startTime > 10.0f)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+
+        switch (request.result)
+        {
+            case UnityWebRequest.Result.ConnectionError:
+            case UnityWebRequest.Result.DataProcessingError:
+                _statusText.text = $"Error connection to the server... Error: {request.error}";
+                _registerButton.interactable = true;
+                break;
+            case UnityWebRequest.Result.ProtocolError:
+                _statusText.text = $": HTTP Error: {request.error}";
+                break;
+            case UnityWebRequest.Result.Success:
+                if (request.downloadHandler.text != "Invalid credential" && request.downloadHandler.text != "Username has been registered")
+                {
+                    _registerButton.interactable = false;
+                    GameAccount returnedAccount = JsonUtility.FromJson<GameAccount>(request.downloadHandler.text);
+                    _statusText.text = "Account has been created";
+                }
+                else
+                {
+                    _statusText.text = $"Username: {username} has been registered";
+                    _registerButton.interactable = true;
+                }
+
+                // Clear
+                _usernameInputField.text = null;
+                _passwordInputField.text = null;
+                break;
+        }
 
         yield return null;
     }
